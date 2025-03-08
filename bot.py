d0c9756666572bd764ea810b0f2cb1af2323514a
um4ru1 <104842639+um4ru1@users.noreply.github.com>
2025-03-08 14:11:20 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,4 +49,4 @@ def handle_text_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Pesan disimpan!"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
