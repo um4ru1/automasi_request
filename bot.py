db482a5e1c33bc5c8fa735eb5d725eb4ead18032
um4ru1 <104842639+um4ru1@users.noreply.github.com>
2025-03-09 18:14:34 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -108,4 +108,4 @@ def store_event(user_id, selected_datetime):
     sheet.append_row([user_id, selected_datetime, "Pesan pending..."])  # Tempat penyimpanan pesan
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
